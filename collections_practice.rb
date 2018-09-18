15b8291c4579faeb0fa6a40b2f0affb8ba7f93cd
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-17 20:04:12 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+require 'pry'
+def sort_array_asc(intarray)
+  intarray.sort
+end
 
+def sort_array_desc(intarray)
+  intarray.sort.reverse
+
+end
+
+def sort_array_char_count(stringarray)
+  stringarray.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  element2 = array[1]
+  element3 = array[2]
+  array[1] = element3
+  array[2] = element2
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(arrayofstrings)
+  newarray = []
+  arrayofstrings.each {|string| string[2] = "$"
+  newarray << string}
+end
+
+def find_a(stringofarray)
+  stringofarray.select {|string| string.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index|
+  if index == 1
+    element
+  else
+    element + "s"
+  end}
+end
